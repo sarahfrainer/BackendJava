4cab78b1d56a7ac15b7adf220b683344f17f8f95
SarahFrainer <sbfrainer@gmail.com>
2024-05-30 17:58:35 -0300

    criação inicial

---
+++ BancoDigital/src/Conta.java
@@ -0,0 +1,43 @@
+public abstract class Conta implements IConta {
+    
+    protected int agencia = 0;
+    protected double saldo = 0;
+    protected int numero = 0;
+    protected Cliente cliente;
+
+    @Override
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    @Override
+    public void transferir(double valor, IConta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo () {
+        return saldo;
+    }
+
+    protected void imprimirInfoComum () {
+        System.out.println(String.format("Titular: %s", cliente.getNome()));
+		System.out.println(String.format("Agencia: %d", agencia));
+		System.out.println(String.format("Numero: %d", numero));
+		System.out.println(String.format("Saldo: %.2f", saldo));
+    }
+
+}
---
+++ BancoDigital/src/ContaPoupança.java
@@ -0,0 +1,11 @@
+public class ContaPoupança extends Conta {
+    public ContaPoupança(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Poupanca ===");
+		super.imprimirInfoComum();
+    }
+}
